d00534f5c62d2c00e3b7e5796bd46d30f64b3e8c
Graham <graham.lim88@gmail.com>
2022-07-02 20:18:46 +0800

    dashboard implemented for merge and pull request

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,16 +1,186 @@
-import React from 'react';
+// import * as React from 'react';
+import React, { useState, useEffect } from 'react';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-import AppliedProjectsCards from '../components/Card/AppliedProjectsCards.jsx';
-import ClosedProjectsCards from '../components/Card/ClosedProjectsCards.jsx';
-import InProgressProjectsCards from '../components/Card/InProgressProjectsCards.jsx';
+import axios from 'axios';
+import BACKEND_URL from '../supportFunctions.js';
+
+const cardsTop = [1, 2, 3];
+const cardsBottom = [7, 8, 9];
+
+const theme = createTheme();
 
 export default function Dashboard() {
+  const [openProjects, setOpenProjects] = useState([]);
+
+  async function getOpenProjects() {
+    try {
+      const results = await axios.get(`${BACKEND_URL}/projects/open`);
+      const { data } = results;
+      console.log(data);
+      console.log(Object.keys(data[0]));
+      const newArray = [];
+      data.forEach((project) => newArray.push(project));
+
+      setOpenProjects(newArray);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getOpenProjects();
+  }, []);
+
   return (
-    <div id="page-container">
-      <h1>Dashboard</h1>
-      <div><AppliedProjectsCards /></div>
-      <div><InProgressProjectsCards /></div>
-      <div><ClosedProjectsCards /></div>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <main>
+        {/* Hero unit */}
+        <Box
+          sx={{
+            bgcolor: 'background.paper',
+            pt: 8,
+            pb: 6,
+          }}
+        >
+          <Container maxWidth="sm">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="text.primary"
+              gutterBottom
+            >
+              Dashboard
+            </Typography>
+            <Typography variant="h5" align="center" color="text.secondary" paragraph>
+              View all Current, Available and Completed Projects
+            </Typography>
+          </Container>
+        </Box>
+
+        <Container sx={{ py: 8 }} maxWidth="md">
+          {/* End hero unit */}
+          <h3> Current Projects </h3>
+          <Grid container spacing={4}>
+            {cardsTop.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card
+                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                >
+                  <CardMedia
+                    component="img"
+                    sx={{
+                      // 16:9
+                      pt: '56.25%',
+                    }}
+                    image="https://source.unsplash.com/random"
+                    alt="random"
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Blah
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can use this section to describe the
+                      content.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">View</Button>
+                    <Button size="small">Edit</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+
+        <Container sx={{ py: 8 }} maxWidth="md">
+          <h3> Available Open Projects </h3>
+          <Grid container spacing={4}>
+            {openProjects.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card
+                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                >
+                  <CardMedia
+                    component="img"
+                    sx={{
+                      // 16:9
+                      pt: '56.25%',
+                    }}
+                    image="https://source.unsplash.com/random"
+                    alt="random"
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {card.name}
+                    </Typography>
+                    <Typography>
+                      Enrolment Deadline:
+                      {' '}
+                      {card.enrolmentDeadline}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">View</Button>
+                    <Button size="small">Edit</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+
+        <Container sx={{ py: 8 }} maxWidth="md">
+          <h3> Completed Projects </h3>
+          <Grid container spacing={4}>
+            {cardsBottom.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card
+                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                >
+                  <CardMedia
+                    component="img"
+                    sx={{
+                      // 16:9
+                      pt: '56.25%',
+                    }}
+                    image="https://source.unsplash.com/random"
+                    alt="random"
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can use this section to describe the
+                      content.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">View</Button>
+                    <Button size="small">Edit</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+    </ThemeProvider>
+
   );
 }
