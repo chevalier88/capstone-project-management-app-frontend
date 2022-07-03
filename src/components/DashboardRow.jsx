c16d6fd2cd4a955e39cef4b17615e6c336ba73aa
Graham <graham.lim88@gmail.com>
2022-07-03 16:11:08 +0800

    one project placeholder rendering in a modal implemented for further work

--- src/components/DashboardRow.jsx
+++ src/components/DashboardRow.jsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
-import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
+import SingleProjectModal from './SingleProjectModal.jsx';
 
 export default function DashboardGridRow({ row }) {
   // if (nested === true) {
@@ -17,22 +17,7 @@ export default function DashboardGridRow({ row }) {
         <Typography gutterBottom variant="h4" component="h2">
           {row.name}
         </Typography>
-        <Typography variant="h5">
-          {row.user_projects.length}
-          /
-          {row.noEngineersRequired}
-          {' '}
-          Engineers Enrolled
-        </Typography>
-        <Typography>
-          Stage:
-          {' '}
-          {row.stage}
-          <br />
-          Delivery Deadline:
-          {' '}
-          {row.deliveryDeadline}
-        </Typography>
+
         <CardMedia
           component="img"
           sx={{
@@ -44,77 +29,28 @@ export default function DashboardGridRow({ row }) {
         />
         <CardContent sx={{ flexGrow: 1 }}>
 
+          <Typography variant="h5">
+            {row.user_projects.length}
+            /
+            {row.noEngineersRequired}
+            {' '}
+            Engineers Enrolled
+          </Typography>
           <Typography>
+            Stage:
+            {' '}
+            {row.stage}
             <br />
-            {row.summary}
+            Delivery Deadline:
+            {' '}
+            {row.deliveryDeadline}
           </Typography>
 
         </CardContent>
         <CardActions>
-          <Button size="small">View</Button>
-          <Button size="small">Edit</Button>
+          <SingleProjectModal rowContent={row} />
         </CardActions>
       </Card>
     </Grid>
   );
 }
-//   return (
-//     <Container sx={{ py: 8 }} maxWidth="md">
-//       {/* End hero unit */}
-//       <h3>
-//         {' '}
-//         {rowName}
-//         {' '}
-//       </h3>
-//       <Grid container spacing={4}>
-//         {projects.map(({ card }) => (
-//           <Grid item key={card.id} xs={12} sm={6} md={4}>
-//             <Card
-//               sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-//             >
-//               <Typography gutterBottom variant="h4" component="h2">
-//                 {card.name}
-//               </Typography>
-//               <Typography variant="h5">
-//                 {card.user_projects.length}
-//                 /
-//                 {card.noEngineersRequired}
-//                 {' '}
-//                 Engineers Enrolled
-//               </Typography>
-//               <Typography>
-//                 Stage:
-//                 {' '}
-//                 {card.stage}
-//                 <br />
-//                 Delivery Deadline:
-//                 {' '}
-//                 {card.deliveryDeadline}
-//               </Typography>
-//               <CardMedia
-//                 component="img"
-//                 sx={{
-//                   // 16:9
-//                   pt: '10.25%',
-//                 }}
-//                 image="https://source.unsplash.com/random"
-//                 alt="random"
-//               />
-//               <CardContent sx={{ flexGrow: 1 }}>
-
-//                 <Typography>
-//                   <br />
-//                   {card.summary}
-//                 </Typography>
-
-//               </CardContent>
-//               <CardActions>
-//                 <Button size="small">View</Button>
-//               </CardActions>
-//             </Card>
-//           </Grid>
-//         ))}
-//       </Grid>
-//     </Container>
-//   );
-// }
