8474630a3ccbbef21d0dda70037203f0033bf951
Graham <graham.lim88@gmail.com>
2022-07-12 17:49:24 +0800

    searchable modals works

--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import SearchBar from '../components/SearchBar.jsx';
 import BACKEND_URL from '../supportFunctions.js';
+import ProfileModalForSearch from '../components/ProfileModalForSearch.jsx';
 
 export default function Search() {
   const [searchQuery, setSearchQuery] = useState('');
@@ -14,7 +15,7 @@ export default function Search() {
       const currentArray = [];
       data.forEach((user) => {
         if (user.accountType === 'engineer') {
-          currentArray.push(user.name);
+          currentArray.push(user);
         }
       });
       console.log(currentArray);
@@ -29,10 +30,11 @@ export default function Search() {
   }, []);
 
   const filterData = (query, data) => {
-    if (!query) {
-      return data;
-    }
-    return data.filter((d) => d.toLowerCase().includes(query));
+    // if (!query) {
+    //   return data;
+    // }
+    const searchResults = data.filter((user) => user.name.toLowerCase().includes(query));
+    return searchResults;
   };
 
   const dataFiltered = filterData(searchQuery, users);
@@ -49,23 +51,8 @@ export default function Search() {
     >
       <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
       <div style={{ padding: 3 }}>
-        {dataFiltered.map((d) => (
-          <div
-            className="text"
-            style={{
-              padding: 5,
-              justifyContent: 'normal',
-              fontSize: 20,
-              color: 'blue',
-              margin: 1,
-              width: '250px',
-              BorderColor: 'green',
-              borderWidth: '10px',
-            }}
-            key={d}
-          >
-            {d}
-          </div>
+        {dataFiltered.map((user) => (
+          <ProfileModalForSearch key={user.id} user={user} />
         ))}
       </div>
     </div>
