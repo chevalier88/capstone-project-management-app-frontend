445c42ce31d7bf2558903329917b0a47a4dc7d2d
quahzhengjie <quahzhengjie@gmail.com>
2022-07-18 22:21:04 +0800

    hompage

---
+++ src/components/Home/ProductValues.jsx
@@ -0,0 +1,89 @@
+import * as React from 'react';
+
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import Typography from './Typography.jsx';
+import ProductValues1 from './HomeAssets/productValues1.svg';
+import ProductValues2 from './HomeAssets/productValues2.svg';
+import ProductValues3 from './HomeAssets/productValues3.svg';
+
+const item = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  px: 5,
+};
+
+function ProductValues() {
+  return (
+    <Box
+      component="section"
+      sx={{ display: 'flex', overflow: 'hidden', bgcolor: 'secondary.light' }}
+    >
+      <Container sx={{
+        mt: 15, mb: 30, display: 'flex', position: 'relative',
+      }}
+      >
+        <Box
+          component="img"
+          src="/static/themes/onepirate/productCurvyLines.png"
+          alt="curvy lines"
+          sx={{ pointerEvents: 'none', position: 'absolute', top: -180 }}
+        />
+        <Grid container spacing={5}>
+          <Grid item xs={12} md={4}>
+            <Box sx={item}>
+              <Box
+                component="img"
+                src={ProductValues1}
+                alt="suitcase"
+                sx={{ height: 55 }}
+              />
+              <Typography variant="h6" sx={{ my: 5 }}>
+                THE BEST TEAM
+              </Typography>
+              <Typography variant="h5">
+                Have an experienced team working on your Web Application, with a few simple steps!
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Box sx={item}>
+              <Box
+                component="img"
+                src={ProductValues2}
+                alt="graph"
+                sx={{ height: 55 }}
+              />
+              <Typography variant="h6" sx={{ my: 5 }}>
+                AWESOME ROI
+              </Typography>
+              <Typography variant="h5">
+                Get the most value out of every dollar spent!
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Box sx={item}>
+              <Box
+                component="img"
+                src={ProductValues3}
+                alt="clock"
+                sx={{ height: 55 }}
+              />
+              <Typography variant="h6" sx={{ my: 5 }}>
+                EXCLUSIVE RATES
+              </Typography>
+              <Typography variant="h5">
+                By registering, you will have access to rates that you will not find anywhere else.
+              </Typography>
+            </Box>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
+}
+
+export default ProductValues;
---
+++ src/components/Home/WithRoot.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import * as React from 'react';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from './Theme.jsx';
+
+export default function withRoot(Component) {
+  function WithRoot(props) {
+    return (
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...props} />
+      </ThemeProvider>
+    );
+  }
+
+  return WithRoot;
+}
