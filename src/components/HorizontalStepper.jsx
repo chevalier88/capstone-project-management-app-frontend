a2f41e2f7c111d3e12cab1422b3b73fe23e51733
Graham <graham.lim88@gmail.com>
2022-07-22 08:52:45 +0800

    minor css for stage changer

--- src/components/HorizontalStepper.jsx
+++ src/components/HorizontalStepper.jsx
@@ -5,47 +5,19 @@ import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
 import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 
 const steps = ['Select campaign settings', 'Create an ad group', 'Create an ad'];
 
 export default function HorizontalStepper() {
   const [activeStep, setActiveStep] = useState(0);
-  // const [skipped, setSkipped] = useState(new Set());
-
-  // const isStepOptional = (step) => step === 1;
-
-  // const isStepSkipped = (step) => skipped.has(step);
 
   const handleNext = () => {
-    // let newSkipped = skipped;
-    // if (isStepSkipped(activeStep)) {
-    //   newSkipped = new Set(newSkipped.values());
-    //   newSkipped.delete(activeStep);
-    // }
-
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    // setSkipped(newSkipped);
   };
 
   const handleBack = () => {
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
   };
-
-  // const handleSkip = () => {
-  //   if (!isStepOptional(activeStep)) {
-  //     // You probably want to guard against something like this,
-  //     // it should never occur unless someone's actively trying to break something.
-  //     throw new Error("You can't skip a step that isn't optional.");
-  //   }
-
-  //   setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  //   setSkipped((prevSkipped) => {
-  //     const newSkipped = new Set(prevSkipped.values());
-  //     newSkipped.add(activeStep);
-  //     return newSkipped;
-  //   });
-  // };
 
   const handleReset = () => {
     setActiveStep(0);
@@ -57,14 +29,7 @@ export default function HorizontalStepper() {
         {steps.map((label) => {
           const stepProps = {};
           const labelProps = {};
-          // if (isStepOptional(index)) {
-          //   labelProps.optional = (
-          //     <Typography variant="caption">Optional</Typography>
-          //   );
-          // }
-          // if (isStepSkipped(index)) {
-          //   stepProps.completed = false;
-          // }
+
           return (
             <Step key={label} {...stepProps}>
               <StepLabel {...labelProps}>{label}</StepLabel>
@@ -73,21 +38,13 @@ export default function HorizontalStepper() {
         })}
       </Stepper>
       {activeStep === steps.length ? (
-        <>
-          <Typography sx={{ mt: 2, mb: 1 }}>
-            All steps completed - you&apos;re finished
-          </Typography>
-          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-            <Box sx={{ flex: '1 1 auto' }} />
-            <Button onClick={handleReset}>Reset</Button>
-          </Box>
-        </>
+        <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+          <Box sx={{ flex: '1 1 auto' }} />
+          <Button onClick={handleReset}>Reset</Button>
+        </Box>
       ) : (
         <>
-          <Typography sx={{ mt: 2, mb: 1 }}>
-            Step
-            {activeStep + 1}
-          </Typography>
+
           <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
             <Button
               color="inherit"
@@ -95,7 +52,7 @@ export default function HorizontalStepper() {
               onClick={handleBack}
               sx={{ mr: 1 }}
             >
-              Back
+              Previous Stage
             </Button>
             <Box sx={{ flex: '1 1 auto' }} />
 
