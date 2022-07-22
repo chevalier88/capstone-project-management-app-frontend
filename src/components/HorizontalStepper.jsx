6b6466e14fea994c197a98e6c8b62a5cf8cc8819
Graham <graham.lim88@gmail.com>
2022-07-22 09:42:08 +0800

    stage changer for engineers works

--- src/components/HorizontalStepper.jsx
+++ src/components/HorizontalStepper.jsx
@@ -1,15 +1,17 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import Box from '@mui/material/Box';
 import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
 import Button from '@mui/material/Button';
+import { UserContext } from './UserContext.jsx';
 
 const steps = ['contracting', 'sourcing', 'in-progress', 'client-review', 'payment-pending', 'completed'];
 
 export default function HorizontalStepper({ stage }) {
   console.log(`current project stage: ${stage}`);
+  const { user } = useContext(UserContext);
   const [activeStep, setActiveStep] = useState((steps.indexOf(stage)));
   console.log((steps.indexOf(stage)));
 
@@ -39,30 +41,35 @@ export default function HorizontalStepper({ stage }) {
           );
         })}
       </Stepper>
-      {activeStep === steps.length ? (
-        <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-          <Box sx={{ flex: '1 1 auto' }} />
-          <Button onClick={handleReset}>Reset</Button>
-        </Box>
-      ) : (
-        <>
-
+      {user.accountType === 'manager'
+      && (
+      <>
+        {activeStep === steps.length ? (
           <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-            <Button
-              color="inherit"
-              disabled={activeStep === 0}
-              onClick={handleBack}
-              sx={{ mr: 1 }}
-            >
-              Previous Stage
-            </Button>
             <Box sx={{ flex: '1 1 auto' }} />
-
-            <Button onClick={handleNext}>
-              {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-            </Button>
+            <Button onClick={handleReset}>Reset</Button>
           </Box>
-        </>
+        ) : (
+          <>
+
+            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+              <Button
+                color="inherit"
+                disabled={activeStep === 0}
+                onClick={handleBack}
+                sx={{ mr: 1 }}
+              >
+                Previous Stage
+              </Button>
+              <Box sx={{ flex: '1 1 auto' }} />
+
+              <Button onClick={handleNext}>
+                {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+              </Button>
+            </Box>
+          </>
+        )}
+      </>
       )}
     </Box>
   );
